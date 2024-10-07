9318a3662d98e94c13ed425097c05b17dda01174
周义竣 <zhouyijun@bwcj.com>
2024-10-07 21:14:16 +0800

    feat: 上传文件

--- src/pages/list/list.vue
+++ src/pages/list/list.vue
@@ -1,9 +1,23 @@
 <template>
   <view> list </view>
-
+  <view>{{ test }}</view>
 </template>
-<script setup></script>
+<script setup>
+import { onLoad } from "@dcloudio/uni-app";
+import { ref } from "vue";
+const test = ref("");
+onLoad(() => {
+  uni
+    .request({
+      url: "http://127.0.0.1:8888/test",
+      method: "POST",
+    })
+    .then((res) => {
+      console.log("res", res);
+      test.value = res.data;
+    });
+  console.log("pages/list,页面栈", getCurrentPages());
+});
+</script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,6 +1,36 @@
 <template>
   <text> my </text>
+  <view> 图片预览：<image :src="imgSrc" /> </view>
+  <button @click="upload" type="default">上传</button>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const imgSrc = ref("");
+
+const upload = function () {
+  uni.chooseImage({
+    success: (res) => {
+      console.log("chooseImage res", res);
+
+      uni.uploadFile({
+        url: "http://127.0.0.1:8888/upload",
+        filePath: res.tempFilePaths[0], // 必须要有
+        name: "file",
+        formData: {
+          file: res.tempFiles[0], // 服务端识别req.file
+        },
+        success: (uploadRes) => {
+          console.log("上传成功", uploadRes);
+          imgSrc.value = JSON.parse(uploadRes.data).url;
+        },
+        fail: (error) => {
+          console.error("上传失败", error);
+        },
+      });
+    },
+  });
+};
+</script>
 
 <style scoped></style>
