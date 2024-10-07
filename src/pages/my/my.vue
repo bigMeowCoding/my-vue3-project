ddd1842a80f21e0d156e82671c22c6dd688dbe68
周义竣 <zhouyijun@bwcj.com>
2024-10-07 21:29:42 +0800

    feat: 多文件上传

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,33 +1,38 @@
 <template>
   <text> my </text>
-  <view> 图片预览：<image :src="imgSrc" /> </view>
+  <view>
+    图片预览：
+
+    <image :key="index" v-for="(item, index) in imgSrcs" :src="item" />
+  </view>
   <button @click="upload" type="default">上传</button>
 </template>
 <script setup>
 import { ref } from "vue";
 
-const imgSrc = ref("");
+const imgSrcs = ref([]);
 
 const upload = function () {
   uni.chooseImage({
     success: (res) => {
       console.log("chooseImage res", res);
-
-      uni.uploadFile({
-        url: "http://127.0.0.1:8888/upload",
-        filePath: res.tempFilePaths[0], // 必须要有
-        name: "file",
-        formData: {
-          file: res.tempFiles[0], // 服务端识别req.file
-        },
-        success: (uploadRes) => {
-          console.log("上传成功", uploadRes);
-          imgSrc.value = JSON.parse(uploadRes.data).url;
-        },
-        fail: (error) => {
-          console.error("上传失败", error);
-        },
-      });
+      for (let i = 0; i < res.tempFilePaths.length; i++) {
+        uni.uploadFile({
+          url: "http://127.0.0.1:8888/upload",
+          filePath: res.tempFilePaths[i], // 必须要有
+          name: "file",
+          formData: {
+            file: res.tempFiles[i], // 服务端识别req.file
+          },
+          success: (uploadRes) => {
+            console.log("上传成功", uploadRes);
+            imgSrcs.value[i] = JSON.parse(uploadRes.data).url;
+          },
+          fail: (error) => {
+            console.error("上传失败", error);
+          },
+        });
+      }
     },
   });
 };
