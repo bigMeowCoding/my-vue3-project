f62599e7b9301a46bde16d30d624deba404909d2
周义竣 <zhouyijun@bwcj.com>
2024-10-08 23:57:13 +0800

    feat: 反馈

--- src/pages/list/list.vue
+++ src/pages/list/list.vue
@@ -1,12 +1,39 @@
 <template>
   <view> list </view>
   <view>{{ test }}</view>
+  <view>
+    <view @click="showGender">选择性别</view>
+    <view>
+      <text>
+        {{ gender === 0 ? "男" : gender === 1 ? "女" : "请选择" }}
+      </text>
+    </view>
+  </view>
 </template>
 <script setup>
 import { onLoad } from "@dcloudio/uni-app";
 import { ref } from "vue";
 const test = ref("");
+const gender = ref();
 onLoad(() => {
+  // uni.showToast({
+  //   title: "123",
+  //   icon: "none",
+  //   mask: "true",
+  // });
+  // uni.showLoading({
+  //   title: "123",
+  //   icon: "none",
+  //   mask: false,
+  // });
+  // uni.showModal({
+  //   title: "123",
+  //   content: "123",
+  //   showCancel: true,
+  //   success: (res) => {
+  //     console.log("res", res);
+  //   },
+  // });
   uni
     .request({
       url: "http://127.0.0.1:8888/test",
@@ -18,6 +45,14 @@ onLoad(() => {
     });
   console.log("pages/list,页面栈", getCurrentPages());
 });
+const showGender = () => {
+  uni.showActionSheet({
+    itemList: ["男", "女"],
+    success: (res) => {
+      gender.value = res.tapIndex;
+    },
+  });
+};
 </script>
 
 <style scoped></style>
