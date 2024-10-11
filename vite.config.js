7e8e41a8a3b0e93eeacd11443b52233ba33e5848
周义竣 <zhouyijun@bwcj.com>
2024-10-11 22:12:00 +0800

    chore: 配置alias

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import uni from '@dcloudio/vite-plugin-uni'
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import uni from "@dcloudio/vite-plugin-uni";
+
 export default defineConfig({
-  plugins: [
-    uni(),
-  ],
-})
+  alias: {
+    "@": "/src",
+  },
+  plugins: [uni()],
+});
