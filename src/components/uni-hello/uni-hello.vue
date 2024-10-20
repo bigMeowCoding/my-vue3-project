17d0c5bc568973583fa40e9aee47c819f436c403
周义竣 <zhouyijun@bwcj.com>
2024-10-20 18:49:46 +0800

    feat:录音

--- src/components/uni-hello/uni-hello.vue
+++ src/components/uni-hello/uni-hello.vue
@@ -1,13 +1,48 @@
+<template>
+  <view>
+    {{ voiceFilePath }}
+  </view>
+  <button @click="startRecord">开始录音</button>
+  <button @click="stopRecord">结束录音</button>
+  <button @click="playRecord">播放录音</button>
+</template>
+
 <script setup>
 import { onLoad } from "@dcloudio/uni-app";
+import { ref } from "vue";
 
-// #ifdef H5
-console.log("h5");
-// #endif
+const recordManager = ref(null);
+const innerAudioContext = uni.createInnerAudioContext();
+const voiceFilePath = ref("");
+// innerAudioContext.src =
+//   "https://lc-sycdn.kuwo.cn/8190d0434c74c89c91cf570316b670ae/6714c74e/resource/n2/6/67/2382874145.mp3?bitrate$128&from=vip";
+innerAudioContext.onPlay(() => {
+  console.log("开始播放");
+});
+recordManager.value = uni.getRecorderManager();
+// innerAudioContext.autoplay = true;
+recordManager.value.onStop((res) => {
+  console.log("res", res);
+  voiceFilePath.value = res.tempFilePath;
+});
+onLoad(() => {
+  // innerAudioContext.play();
+});
+
+const startRecord = () => {
+  recordManager.value.start();
+};
+const stopRecord = () => {
+  recordManager.value.stop();
+};
+const playRecord = () => {
+  console.log("voiceFilePath", voiceFilePath.value);
+
+  if (voiceFilePath.value) {
+    innerAudioContext.src = voiceFilePath.value;
+    innerAudioContext.play();
+  }
+};
 </script>
-
-<template>
-  <view> hello world </view>
-</template>
 
 <style scoped></style>
--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -10,6 +10,18 @@
 <script setup>
 import { ref } from "vue";
 
+// const innerAudioContext = uni.createInnerAudioContext();
+// innerAudioContext.autoplay = true;
+// innerAudioContext.src =
+//   "https://web-ext-storage.dcloud.net.cn/uni-app/ForElise.mp3";
+// innerAudioContext.onPlay(() => {
+//   console.log("开始播放");
+// });
+// innerAudioContext.onError((res) => {
+//   console.log(res.errMsg);
+//   console.log(res.errCode);
+// });
+
 const imgSrcs = ref([]);
 
 const upload = function () {
